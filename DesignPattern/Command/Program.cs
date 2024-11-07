9fb6861492c93ba2be028a5bfd2297edd88ca02f
douglas <douglas.rittono@live.com>
2024-11-06 21:32:41 -0300

    Command e Strategy

---
+++ DesignPattern/Command/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controle controle = new Controle();
+            //controle.EnviarComandoSimples(new SimplesCOmando("Dizer oi"));
+
+            Receiver receiver = new Receiver();
+            controle.EnviarComandoComplexo(new ComplexoComando(receiver, "Abastecer o carro", "Calibrar o pneu"));
+
+
+            controle.Fazer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPattern/Command/SimplesCOmando.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class SimplesCOmando : ICommand
+    {
+        private string _solicitacao = string.Empty;
+
+        public SimplesCOmando(string solicitacao) 
+        {
+            this._solicitacao=solicitacao;
+        }
+
+        public void Executar()
+        {
+            Console.WriteLine("Estou executando comando de " + this._solicitacao);
+        }
+    }
+}
