8157b8c94383934425ad4f8262966c2184a4507e
DeltragonKH <deltragon@gmail.com>
2024-11-13 20:09:53 -0300

    mediator

---
+++ DesignPattern/Mediator/Colaborador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public abstract class Colaborador
+    {
+        protected IMediador mediador;
+
+        public Colaborador(IMediador mediador)
+        {
+            this.mediador = mediador;
+        }
+
+        public abstract void EnviarMensagem(string mensagem);
+        public abstract void ReceberMensagem(string mensagem);
+    }
+
+    // Colaborador A
+    public class ColaboradorA : Colaborador
+    {
+        public ColaboradorA(IMediador mediador) : base(mediador) { }
+
+        public override void EnviarMensagem(string mensagem)
+        {
+            Console.WriteLine("Colaborador A enviando mensagem: " + mensagem);
+            mediador.Comunicar(mensagem, this);
+        }
+
+        public override void ReceberMensagem(string mensagem)
+        {
+            Console.WriteLine("Colaborador A recebeu mensagem: " + mensagem);
+        }
+    }
+
+    // Colaborador B
+    public class ColaboradorB : Colaborador
+    {
+        public ColaboradorB(IMediador mediador) : base(mediador) { }
+
+        public override void EnviarMensagem(string mensagem)
+        {
+            Console.WriteLine("Colaborador B enviando mensagem: " + mensagem);
+            mediador.Comunicar(mensagem, this);
+        }
+
+        public override void ReceberMensagem(string mensagem)
+        {
+            Console.WriteLine("Colaborador B recebeu mensagem: " + mensagem);
+        }
+    }
+}
---
+++ DesignPattern/Mediator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar o mediador
+            MediadorConcreto mediador = new MediadorConcreto();
+
+            // Criar os colaboradores e passar o mediador para eles
+            ColaboradorA colaboradorA = new ColaboradorA(mediador);
+            ColaboradorB colaboradorB = new ColaboradorB(mediador);
+
+            // Registrar os colaboradores no mediador
+            mediador.SetColaboradorA(colaboradorA);
+            mediador.SetColaboradorB(colaboradorB);
+
+            // Testar a comunicação
+            colaboradorA.EnviarMensagem("Olá, Colaborador B!");
+            colaboradorB.EnviarMensagem("Olá, Colaborador A!");
+        }
+    }
+}
+
+    
+
